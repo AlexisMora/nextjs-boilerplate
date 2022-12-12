0b3e37b7c1d4374eea2171ba079c49a537ed9925
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-12-12 21:22:37 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  const x = '';
   return (
     <div className={styles.container}>
       <Head>
